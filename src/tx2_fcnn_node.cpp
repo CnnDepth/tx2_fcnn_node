08f09550ae082f592de84141e285ca0a2464380a
avbokovoy <bokovoy@isa.ru>
2019-06-05 09:04:27 +0000

    Minor tx2_fcnn_node.cpp cleanup

--- src/tx2_fcnn_node.cpp
+++ src/tx2_fcnn_node.cpp
@@ -26,18 +26,7 @@
 
 const int DEFAULT_CAMERA_HEIGHT = 640;
 
-/*class Logger : public nvinfer1::ILogger
-{
-  void log(nvinfer1::ILogger::Severity severity, const char* msg) override
-  {
-    std::cout << msg << std::endl;
-  }
-};*/
-
 Logger gLogger;
-
-//nvinfer1::ILogger gLogger;
-
 
 cudaError_t cudaPreImageNetMean(float4* input, size_t inputWidth, size_t inputHeight,
 				             float* output, size_t outputWidth, size_t outputHeight, const float3& mean_value, cudaStream_t stream);
@@ -147,29 +136,6 @@ int main( int argc, char** argv )
     return -1;
   }
 
-  
-
-//==========================
-
-
-
-//  std::string pipeline = get_tegra_pipeline( 1280, 720, 30 );
-//  cv::VideoCapture vCap(0);
-//  cv::VideoCapture vCap( pipeline, cv::CAP_GSTREAMER );
-
-//   cv::VideoCapture vCap("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, framerate=120/1, format=(string)NV12 ! nvvidconv ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! appsink", cv::CAP_GSTREAMER);
-
-//  cv::VideoCapture vCap("v4l2src device=/dev/video1 ! video/x-raw, width=(int)3840, height=(int)2160,format=(string)I420, framerate=(fraction)30/1 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
-//  ROS_INFO( "Using pipeline: %s", pipeline.c_str() );
-
-/*
-  if( !vCap.isOpened() )
-  {
-    ROS_INFO( "Error!" );
-    vCap.release();
-    return -1;
-  }
-*/
   ros::Publisher ciPub = nh.advertise<sensor_msgs::CameraInfo>( "/rgb/camera_info", 1 );
   sensor_msgs::CameraInfo ciMsg;
   ciMsg.header = std_msgs::Header();
@@ -177,15 +143,7 @@ int main( int argc, char** argv )
   ciMsg.height = 480;
   ciMsg.width  = 640;
   ciMsg.distortion_model = "plumb_bob";
-//  double distor[5] = {0.262383, -0.953104, -0.005358, 0.002628, 1.163314};
-//  ciMsg.D = {0.262383, -0.953104, -0.005358, 0.002628, 1.163314};
-//  double k[9] = {517.306408, 0, 318.643040, 0, 517.469215, 255.313989, 0, 0, 1};
-//  ciMsg.K = {517.306408, 0, 318.643040, 0, 517.469215, 255.313989, 0, 0, 1};
-//  ciMsg.R = {1, 0, 0, 0, 1, 0, 0, 0, 1};
-//  ciMsg.P = { 535.4, 0, 320.1, 0, 0, 539.2, 247.05, 0.0, 0.0, 1.0 };
-
   ciMsg.D = {0.11754636857648042, -0.20796214251890827, 0.003951824147288686, 0.0011981225998639721, 0.0};
-//  double k[9] = {517.306408, 0, 318.643040, 0, 517.469215, 255.313989, 0, 0, 1};
   ciMsg.K = {239.12800192487694, 0.0, 164.5580937136966, 0.0, 319.0746579524651, 121.00594783717585, 0.0, 0.0, 1.0};
   ciMsg.R = {1, 0, 0, 0, 1, 0, 0, 0, 1};
   ciMsg.P = { 241.8467254638672, 0.0, 165.24418493136181, 0.0, 0.0, 324.6019287109375, 121.7070042868836, 0.0, 0.0, 0.0, 1.0, 0.0 };
@@ -198,9 +156,7 @@ int main( int argc, char** argv )
   diMsg.height = 480;
   diMsg.width  = 640;
   diMsg.distortion_model = "plumb_bob";
-//  double distor[5] = {0.262383, -0.953104, -0.005358, 0.002628, 1.163314};
   diMsg.D = {0.11754636857648042, -0.20796214251890827, 0.003951824147288686, 0.0011981225998639721, 0.0};
-//  double k[9] = {517.306408, 0, 318.643040, 0, 517.469215, 255.313989, 0, 0, 1};
   diMsg.K = {239.12800192487694, 0.0, 164.5580937136966, 0.0, 319.0746579524651, 121.00594783717585, 0.0, 0.0, 1.0};
   diMsg.R = {1, 0, 0, 0, 1, 0, 0, 0, 1};
   diMsg.P = { 241.8467254638672, 0.0, 165.24418493136181, 0.0, 0.0, 324.6019287109375, 121.7070042868836, 0.0, 0.0, 0.0, 1.0, 0.0 };
@@ -212,8 +168,6 @@ int main( int argc, char** argv )
   image_transport::Publisher  depthPub = it.advertise( "/depth/image", 1 );
 
   cv::Mat frame;
-  
-//  ImageConverter imgConv(nh);
   std::chrono::time_point<std::chrono::system_clock> start, end;
 
 
@@ -222,26 +176,6 @@ int main( int argc, char** argv )
 
   while( nh.ok() )
   {
-/*    start = std::chrono::system_clock::now();
-
-    vCap >> frame;
-
-//    imgConv.publish( frame );
-//    cv::imshow("asd", frame);
-//    cv::waitKey(1);
-
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage( std_msgs::Header(), "bgr8", frame ).toImageMsg();
-
-
-    pub.publish(msg);
-
-    ros::spinOnce();
-    loop_rate.sleep();
-
-    end = std::chrono::system_clock::now();
-    int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-    ROS_INFO( "vCap Time: %d", elapsed_seconds );
-*/
     void* imgCPU  = NULL;
     void* imgCUDA = NULL;
     void* imgRGBA = NULL;
@@ -263,10 +197,6 @@ int main( int argc, char** argv )
 
 
 
-
-
-
-
     const float3 mean_value = make_float3( 123.0, 115.0, 101.0 );
 
     if( CUDA_FAILED( cudaPreImageNetMean( (float4*)imgRGBA, 640, 480, (float*)imgRGB, 640, 480, mean_value, stream ) ) )
@@ -279,16 +209,7 @@ int main( int argc, char** argv )
     void* bindings[] = {imgRGB, outImageCUDA};
     context->execute( 1, bindings );
 
-//    CUDA_FAILED( cudaDivideByMaxValue( outImageCUDA, (float*)dividedDepth, camera->GetWidth(), camera->GetHeight(), 10 ) );
     cv::Mat outDepth( camera->GetHeight(), camera->GetWidth(), CV_32FC1, outImageCUDA );
-
-//    std::cout << outDepth << std::endl;
-
-    double min, max;
-    cv::minMaxLoc( outDepth, &min, &max );
-    ROS_INFO( "-- min: %d max: %d", min, max  );
-
-//    outDepth.convertTo( outDepth, CV_8UC1, 255.0/(max-min), -min * ( 255.0/(max-min) ));
 
     sensor_msgs::ImagePtr depthMsg = cv_bridge::CvImage( std_msgs::Header(), sensor_msgs::image_encodings::TYPE_32FC1, outDepth ).toImageMsg();
 
@@ -312,8 +233,6 @@ int main( int argc, char** argv )
     ros::spinOnce();
     loop_rate.sleep();
   }
-
-//  vCap.release();
 
   return 0;
 }
