ba7d65660f03f0e86f251fb3b2b9859aa337bc03
kirillMouraviev <kirill.mouraviev@yandex.ru>
2019-06-10 23:13:52 +0300

    Fix bugs; add paths to CMakeLists.txt

--- src/tx2_fcnn_node.cpp
+++ src/tx2_fcnn_node.cpp
@@ -238,7 +238,7 @@ int main( int argc, char** argv )
     {
       ROS_ERROR( "Failed to preprocess" );
 
-      return -1;
+      //return -1;
     }
 
     void* bindings[] = {imgRGB, outImageCUDA};
